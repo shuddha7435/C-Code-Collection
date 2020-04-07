2088e1ef882de3a06dbd1176e262a73e3d43871b
Shuddha Chowdhury <shuddho.aiub@gmail.com>
2020-04-07 13:25:22 -0500

    Pointer-Code
    Added pointer code

---
+++ Pointer_Start.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int x = 8;
+    int y = 10;
+
+    printf("Value of x is %d\n", x);
+    printf("Address of x is %p\n", &x);
+    printf("Value of y is %d\n", y);
+    printf("Address of y is %p\n", &y);
+    return 0;
+}
